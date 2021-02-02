11a6d610622525646f5ad2209687e00a15a517fe
Kirill Pobedonostsev <pobedonoscevkirill@gmail.com>
2021-02-03 01:13:05 +0300

    Formatted whole code and changed main tests

--- dDList/DoubleLinkedList.h
+++ dDList/DoubleLinkedList.h
@@ -4,80 +4,85 @@
 #include <iostream>
 
 class DoubleLinkedList {
-private:
-    struct Node {
-        int item_;
-        Node *prev_;
-        Node *next_;
-
-        Node(int item, Node *prev = nullptr, Node *next = nullptr) :
-                item_(item), prev_(prev), next_(next) {};
-    };
-
-    Node *head_;
-    Node *tail_;
-    int count_;
-
-    Node *head() const;
-
-    Node *tail() const;
-
-    void insertHead(Node *x);
-
-    void insertTail(Node *x);
-
-    void deleteNode(Node *x);
-
-    Node *searchNode(int item) const;
-
-    Node *replaceNode(Node *x, int item);
-
-    void swap(DoubleLinkedList &src);
-
 public:
-    DoubleLinkedList() :
-            count_(0), head_(nullptr), tail_(nullptr) {};
+  DoubleLinkedList():
+          head_(nullptr),
+          tail_(nullptr),
+          count_(0)
+  {};
 
-    DoubleLinkedList(const DoubleLinkedList &src);
+  DoubleLinkedList(const DoubleLinkedList &src);
 
-    DoubleLinkedList(DoubleLinkedList &&src);
+  DoubleLinkedList(DoubleLinkedList &&src);
 
-    ~DoubleLinkedList();
+  ~DoubleLinkedList();
 
-    DoubleLinkedList &operator=(const DoubleLinkedList &src);
+  DoubleLinkedList &operator=(const DoubleLinkedList &src);
 
-    DoubleLinkedList &operator=(DoubleLinkedList &&src);
+  DoubleLinkedList &operator=(DoubleLinkedList &&src);
 
-    void insertHead(int item);
+  bool operator==(const DoubleLinkedList &another);
 
-    void insertTail(int item);
+  friend std::ostream &operator<<(std::ostream &stream, const DoubleLinkedList &list);
 
-    int count() const;
+  void insertHead(int item);
 
-    int headItem() const;
+  void insertTail(int item);
 
-    int &headItem();
+  int count() const;
 
-    int tailItem() const;
+  int headItem() const;
 
-    int &tailItem();
+  int &headItem();
 
-    bool deleteHead();
+  int tailItem() const;
 
-    bool deleteTail();
+  int &tailItem();
 
-    bool deleteItem(const int item, bool all = false);
+  bool deleteHead();
 
-    bool replaceItem(const int oldItem, const int newItem, bool all = false);
+  bool deleteTail();
 
-    bool searchItem(const int item) const;
+  bool deleteItem(int item, bool all = false);
 
-    friend std::ostream &operator<<(std::ostream &stream, const DoubleLinkedList &list);
+  bool replaceItem(int oldItem, int newItem, bool all = false);
 
-    bool operator==(const DoubleLinkedList &another);
+  bool searchItem(int item) const;
 
-    void addListIntoTail(DoubleLinkedList &another);
+  void addListIntoTail(DoubleLinkedList &another);
+
+private:
+  struct Node {
+    int item_;
+    Node *prev_;
+    Node *next_;
+
+    Node(int item, Node *prev = nullptr, Node *next = nullptr):
+            item_(item),
+            prev_(prev),
+            next_(next)
+    {};
+  };
+
+  Node *head_;
+  Node *tail_;
+  int count_;
+
+  Node *head() const;
+
+  Node *tail() const;
+
+  void insertHead(Node *x);
+
+  void insertTail(Node *x);
+
+  void deleteNode(Node *x);
+
+  Node *searchNode(int item) const;
+
+  Node *replaceNode(Node *x, int item);
+
+  void swap(DoubleLinkedList &src);
 };
-
 
 #endif //DDLIST_DOUBLELINKEDLIST_H
--- dDList/main.cpp
+++ dDList/main.cpp
@@ -1,45 +1,47 @@
 #include "DoubleLinkedList.h"
 
 int main() {
-    try {
-        std::cout << "Hello, World!" << std::endl;
-        DoubleLinkedList list1, list2;
-        std::cout << "Empty list1 -> " << list1;
-        list1.insertHead(10);
-        std::cout << "Insert 10 into list1 -> " << list1;
-        list2.insertHead(10);
-        std::cout << "Insert 10 into list1 -> " << list1;
-        DoubleLinkedList list3(list2);
-        std::cout << list1;
-        std::cout << (list1 == list2) << '\n';
-        list1.insertHead(11);
-        list2.insertHead(12);
-        std::cout << (list1 == list2) << '\n';
-        std::cout << list1;
-        list1.insertTail(5);
-        std::cout << list1;
-        list1.insertTail(5);
-        std::cout << list1;
-        list1.insertTail(5);
-        std::cout << list1;
-        list1.insertTail(10);
-        std::cout << list1;
-        list1.insertTail(10);
-        std::cout << list1;
-        list1.deleteItem(10);
-        std::cout << list1;
-        list1.deleteItem(10, true);
-        std::cout << list1;
-        list1.replaceItem(5, 7);
-        std::cout << list1;
-        list1.replaceItem(5, 7, true);
-        std::cout << list1;
-        list1.addListIntoTail(list2);
-        std::cout << list1;
-    }
-    catch (const char *msg) {
-        std::cerr << "Произошла ошибка: " << msg;
-    }
+  try {
+    DoubleLinkedList list1, list2;
+    std::cout << "Empty list1 -> " << list1 << '\n';
+    list1.insertHead(10);
+    std::cout << "Insert 10 into head of list1 -> " << list1 << '\n';
+    list2.insertHead(10);
+    std::cout << "Insert 10 into head of list2 -> " << list2 << '\n';
+    DoubleLinkedList list3(list2);
+    std::cout << "list3(list2) -> " << list3 << '\n';
+    std::cout << "list1 == list2 -> " << (list1 == list2) << '\n';
+    list1.insertHead(11);
+    list2.insertHead(12);
+    std::cout << "list1 -> " << list1 << '\n';
+    std::cout << "list2 -> " << list2 << '\n';
+    std::cout << "list1 == list2 -> " << (list1 == list2) << '\n';
+    list1.insertTail(5);
+    std::cout << "Insert 5 into tail of list1 -> " << list1 << '\n';
+    list1.insertTail(5);
+    list1.insertTail(5);
+    std::cout << "Insert 5, 5 into tail of list1 -> " << list1 << '\n';
+    list1.insertTail(10);
+    list1.insertTail(10);
+    std::cout << "Insert 10, 10 into tail of list1 -> " << list1 << '\n';
+    list1.deleteItem(10);
+    std::cout << "Delete 10 from list1 -> " << list1 << '\n';
+    list1.deleteItem(10, true);
+    std::cout << "Delete 10 from list1 with all=true -> " << list1 << '\n';
+    list1.replaceItem(5, 7);
+    std::cout << "Replace 5 with 7 -> " << list1 << '\n';
+    list1.replaceItem(5, 7, true);
+    std::cout << "Replace 5 with 7 with all=true -> " << list1 << '\n';
+    std::cout << "list2 -> " << list2 << '\n';
+    list1.addListIntoTail(list2);
+    std::cout << "list1 add list2 -> " << list1 << '\n';
+    std::cout << "list2 -> " << list2 << '\n';
+    list2.tailItem();
+  }
+  catch (const char *msg) {
+    std::cerr << "Произошла ошибка: " << msg << '\n';
+    return 2;
+  }
 
-    return 0;
+  return 0;
 }
